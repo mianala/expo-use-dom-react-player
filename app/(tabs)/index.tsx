4b83886c353aa836d94b890edc3e736dae8eaa79
mianala <mianalaloharano@gmail.com>
2024-12-13 18:33:01 +0300

    refactor: simplify VideoPlayer component rendering

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,29 +8,6 @@ import VideoPlayer from "@/components/react-player"
 
 export default function HomeScreen() {
   return (
-    <View style={{ height: 100, width: 100 }}>
-      <VideoPlayer
-        url={"https://www.youtube.com/watch?v=jU4_1vpjahw&t=3656s"}
-      />
-    </View>
+    <VideoPlayer url={"https://www.youtube.com/watch?v=jU4_1vpjahw&t=3656s"} />
   )
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
-  },
-})
